0872960ce1df1ce86b6f49e32e552888e385b1c3
Daria Mayakova <mayakova.daria92@gmail.com>
2024-04-24 16:09:47 +0300

    Initial commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        // Дані про товари
         String product1 = "smartphone";
-        int days1 = 5;
-        double totalSales1 = 12153.41;
-        double salesPerDay1 = 2430.68;
+        int quantity1 = 10;
+        double price1 = 1215.34;
         String product2 = "laptop";
-        int days2 = 7;
-        double totalSales2 = 10486.85;
-        double salesPerDay2 = 1498.12;
-        System.out.printf("Product No 1: " + product1);
-        System.out.printf("total sales for " + days1 + " days is EUR " + totalSales1);
-        System.out.printf("sales by day is EUR " + salesPerDay1);
-        System.out.printf("Product No 2: " + product2);
-        System.out.printf("total sales for " + days2 + " days is EUR " + totalSales2);
-        System.out.printf("sales by day is EUR " + salesPerDay2);
+        int quantity2 = 7;
+        double price2 = 1498.12;
+        double totalSales1 = quantity1 * price1;
+        double averageSales1 = totalSales1 / 5;
+        double totalSales2 = quantity2 * price2;
+        double averageSales2 = totalSales2 / 7;
+        System.out.printf("Product No 1: %s%n", product1);
+        System.out.printf("total sales for 5 days is EUR %.2f%n", totalSales1);
+        System.out.printf("sales by day is EUR %.2f%n", averageSales1);
+        System.out.printf("Product No 2: %s%n", product2);
+        System.out.printf("total sales for 7 days is EUR %.2f%n", totalSales2);
+        System.out.printf("sales by day is EUR %.2f%n", averageSales2);
     }
 }
