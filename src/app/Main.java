a796faea82e6466c83b9fb8d6d25f18fc8f8efea
Daria Mayakova <mayakova.daria92@gmail.com>
2024-04-24 15:37:10 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String product1 = "smartphone";
+        int days1 = 5;
+        double totalSales1 = 12153.41;
+        double salesPerDay1 = 2430.68;
+        String product2 = "laptop";
+        int days2 = 7;
+        double totalSales2 = 10486.85;
+        double salesPerDay2 = 1498.12;
+        System.out.println("Product No 1: " + product1);
+        System.out.println("total sales for " + days1 + " days is EUR " + totalSales1);
+        System.out.println("sales by day is EUR " + salesPerDay1);
+        System.out.println("Product No 2: " + product2);
+        System.out.println("total sales for " + days2 + " days is EUR " + totalSales2);
+        System.out.println("sales by day is EUR " + salesPerDay2);
+    }
+}
