f474ce73837bd648e5a31aca3cb5e3c1a1c802b6
Daria Mayakova <mayakova.daria92@gmail.com>
2024-04-24 18:59:43 +0300

    Initial commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Дані про товари
         String product1 = "smartphone";
         int quantity1 = 10;
         double price1 = 1215.34;
@@ -11,10 +10,10 @@ public class Main {
         double averageSales1 = totalSales1 / 5;
         double totalSales2 = quantity2 * price2;
         double averageSales2 = totalSales2 / 7;
-        System.out.printf("Product No 1: %s%n", product1);
+        System.out.println("Product No 1: " + product1);
         System.out.printf("total sales for 5 days is EUR %.2f%n", totalSales1);
-        System.out.printf("sales by day is EUR %.2f%n", averageSales1);
-        System.out.printf("Product No 2: %s%n", product2);
+        System.out.printf("sales by day is EUR %.2f%n%n", averageSales1);
+        System.out.println("Product No 2: " + product2);
         System.out.printf("total sales for 7 days is EUR %.2f%n", totalSales2);
         System.out.printf("sales by day is EUR %.2f%n", averageSales2);
     }
